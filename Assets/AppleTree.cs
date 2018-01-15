29ce7d70955679814949049cd0aaa92d72c534e1
sirFortesque <6eefree@gmail.com>
2018-01-15 17:33:54 +0200

    ok

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 8; 
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.02f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // difficulty level of the game
+    public List<int> dif = new List<int>();
+
+    // Use this for initialization
+    void Start () {
+        dif.Add(0);
+        //speed.Add(10f);
+		//Dropping apples every second
+        Invoke("DropApple", 2f);
+	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Changing difficulty depending on time
+
+
+
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos; 
+       
+        // Changing Direction
+        if ( pos.x < -leftAndRightEdge )
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        // Changing direction randomly
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
